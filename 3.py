a42873fdacf6611e492b82c15efd71bed3762b85
Sridevi45 <61419329+Sridevi45@users.noreply.github.com>
2022-11-21 16:23:38 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,1 +1,14 @@
-
+def check(p):
+  while(p>0):
+    c=p%10
+    if(c==3 or c==4 or c==7):
+      return false
+   p//=10
+ return True
+p=int(input())
+c,count=1,1
+while(count<p):
+  c+=1
+  if(check(c)):
+    count+=1
+print(c)
